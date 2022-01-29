1a40f85880eb48b353e48f30337c03c10f2e76db
brandongasser <bgasser12@gmail.com>
2022-01-29 13:14:15 -0600

    move and shoot (haven't seen it work)

--- base_client.py
+++ base_client.py
@@ -28,13 +28,31 @@ class Client(UserClient):
             i += 1
         return -1
 
+    def movement(self, turn, actions: Action, game_board, partition_grid: PartitionGrid, shooter: Shooter) -> None:
+        # This is the list that contains all the objects on the map your player can see
+        map_objects = partition_grid.get_all_objects()
+        # This is a tuple that represents the position 1 unit in front of where the player
+        forward_position = (shooter.hitbox.middle[0] + shooter.hitbox.width + math.cos(math.radians(shooter.heading)),
+                            shooter.hitbox.middle[1] + shooter.hitbox.height + math.sin(math.radians(shooter.heading)))
+        object_in_front = None
+        if forward_position[0] < game_board.width and forward_position[1] < game_board.height:
+            object_in_front = partition_grid.find_object_coordinates(forward_position[0], forward_position[1])
+        if self.prev_location != shooter.hitbox.middle:
+            angle = angle_to_point(shooter, game_board.center)
+            actions.set_move(int(angle), shooter.max_speed)
+            self.prev_location = shooter.hitbox.middle
+        elif object_in_front or 0 <= forward_position[0] <= 500 or 0 <= forward_position[1] <= 500 \
+                and self.prev_location != game_board.center:
+            #shooter.heading = 0
+            actions.set_move((shooter.heading + 90) % 360, 10)
+
     def shoot(self, turn, actions: Action, game_board, partition_grid: PartitionGrid, shooter: Shooter) -> None:
         things = list(partition_grid.get_all_objects())
         index = self.find_player(things, shooter)
         if (index >= 0):
             opponent = things[index]
             actions.set_shoot(heading = player_utils.angle_to_point(shooter, opponent.hitbox.middle))
-        print("\nturn %d:\nplayer index: %d\nopponent: %s\n\n\n" % (turn, index, things[index] if index >= 0 else "no opponent found"))
 
     def take_turn(self, turn, actions: Action, game_board, partition_grid: PartitionGrid, shooter: Shooter) -> None:
-        shoot(turn, actions, game_board, partition_grid, shooter)
+        self.shoot(turn, actions, game_board, partition_grid, shooter)
+        self.movement(turn, actions, game_board, partition_grid, shooter)
